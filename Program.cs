a08db25dc1137b9218143bc9ef3f02c2654c4ebd
opilane <opilane@HP-CZC2349HV1>
2024-09-27 14:12:40 +0300

    test2

--- Program.cs
+++ Program.cs
@@ -7,35 +7,28 @@ namespace JalgpalliMang
     {
         static void Main(string[] args)
         {
-            // Create stadium
             Stadium stadium = new Stadium(35, 15);
 
-            // Create teams
             Team homeTeam = new Team("Home Team");
             Team awayTeam = new Team("Away Team");
 
-            // Add players to home team
-            homeTeam.AddPlayer(new Player("A")); // Shortened names for display
+            homeTeam.AddPlayer(new Player("A")); 
             homeTeam.AddPlayer(new Player("A"));
             homeTeam.AddPlayer(new Player("A"));
 
-            // Add players to away team
             awayTeam.AddPlayer(new Player("B"));
             awayTeam.AddPlayer(new Player("B"));
             awayTeam.AddPlayer(new Player("B"));
 
-            // Create game
             Game game = new Game(homeTeam, awayTeam, stadium);
 
-            // Start the game
             game.Start();
 
-            // Simulate some game moves
-            for (int i = 0; i < 3000; i++) // Simulate 10 turns
+            for (int i = 0; i < 3000; i++) 
             {
                 game.Move();
                 DisplayGameState(game);
-                System.Threading.Thread.Sleep(700); // Pause for a second between moves
+                System.Threading.Thread.Sleep(700); 
             }
 
             static void DisplayGameState(Game game)
@@ -51,29 +44,26 @@ namespace JalgpalliMang
                 {
                     for (int x = 0; x < game.Stadium.Width; x++)
                     {
-                        char displayChar = ' '; // Default to empty space
+                        char displayChar = ' '; 
 
-                        // Check if the ball is at the current position
                         if ((int)game.Ball.X == x && (int)game.Ball.Y == y)
-                            displayChar = 'O'; // Ball
+                            displayChar = 'O'; 
 
-                        // Check for home players
                         foreach (var player in game.HomeTeam.Players)
                         {
                             if ((int)player.X == x && (int)player.Y == y)
-                                displayChar = 'H'; // Home player
+                                displayChar = 'H'; 
                         }
 
-                        // Check for away players
                         foreach (var player in game.AwayTeam.Players)
                         {
                             if ((int)player.X == x && (int)player.Y == y)
-                                displayChar = 'A'; // Away player
+                                displayChar = 'A'; 
                         }
 
                         Console.Write(displayChar);
                     }
-                    Console.WriteLine(); // New line for each row
+                    Console.WriteLine(); 
                 }
             }
         }
